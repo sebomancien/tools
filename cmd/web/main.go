d4432a45062f6c7c1b3d42899193020d01cc2c88
Sebastien Gravel <sgravel@brioconcept.com>
2024-09-28 16:53:39 -0400

    Added home web page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("POST /api/v1/bin2c", handler.PostHandler)
+	router.HandleFunc("GET /", handler.GetHandler)
 
 	fmt.Println("Starting API server on port 8080")
 	http.ListenAndServe(":8080", router)
