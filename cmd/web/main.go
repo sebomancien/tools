18ee5e2a1779ae6789adb139519fc13f61e37c57
Sebastien Gravel <sgravel@brioconcept.com>
2024-09-30 22:03:52 -0400

    Placed bin2c in subfolders

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/sebomancien/tools/internal/handler"
+	"github.com/sebomancien/tools/internal/bin2c"
 )
 
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("GET /bin2c/{$}", handler.GetHandler)
-	router.HandleFunc("POST /bin2c/convert/{$}", handler.ConvertHandler)
+	router.HandleFunc("GET /bin2c/{$}", bin2c.GetHandler)
+	router.HandleFunc("POST /bin2c/convert/{$}", bin2c.ConvertHandler)
 
 	fmt.Println("Starting API server on port http://localhost:8080/")
 	http.ListenAndServe(":8080", router)
--- internal/bin2c/handler.go
+++ internal/bin2c/handler.go
@@ -1,4 +1,4 @@
-package handler
+package bin2c
 
 import (
 	"fmt"
